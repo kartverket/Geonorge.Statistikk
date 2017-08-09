7b411b50a99e2728eb4219e8952c9c7d46a8f41c
Håvard Vidme <havard.vidme@gmail.com>
2017-08-09 10:44:19 +0200

    Added custom styles to 'Heading' and 'Navigation'

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import './Heading.css'
+
 class Heading extends Component {
   render () {
     const { title } = this.props
     return (
-      <div className="page-header">
-        <h2>{ title }</h2>
+      <div className="heading">
+        <h1>{ title }</h1>
       </div>
     )
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
+import './Navigation.css'
+
 class Navigation extends Component {
   render () {
     return (
-      <div className="container">
-        <nav className="well well-sm">
-          <NavLink activeClassName="btn-info" className="btn btn-sm btn-default" exact to="/">Hjem</NavLink>
-          <span>&nbsp;|&nbsp;</span>
-          <NavLink activeClassName="btn-info" className="btn btn-sm btn-default" to="/nedlastinger">Nedlastinger</NavLink>
-        </nav>
-      </div>
+      <nav className="navigation">
+        <NavLink activeClassName="active" className="navigation-item" exact to="/">Hjem</NavLink>
+        <NavLink activeClassName="active" className="navigation-item" to="/nedlastinger">Nedlastinger</NavLink>
+      </nav>
     )
   }
 }
